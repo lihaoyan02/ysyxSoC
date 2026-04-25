efd07b1c8ae3f3ec1992400483f3712af2acc631
100018556-Li Haoyan <1620774239@qq.com>
2026-04-25 22:41:50 +0800

    QPI for psram

--- perip/psram/psram.v
+++ perip/psram/psram.v
@@ -5,23 +5,28 @@ module psram(
 );
 
   reg [2:0] state;
-  // reg [1:0] rstate;
+  reg [1:0] rstate; //for qpi
+  reg qpi_flag;
+  initial begin
+    qpi_flag = 0; // qpi disabled initially
+  end
+
   localparam IDLE = 3'b0, ADDR_READ=3'b1, WAIT=3'b10, DATA_TRANS_HIGH=3'b11,
-    DATA_TRANS_LOW = 3'b100, DATA_RECV_HIGH=3'b101, DATA_RECV_LOW=3'b110;
-  // localparam RIDLE=2'b00, READ_HIGH = 2'b1, READ_LOW = 2'b10;
+    DATA_TRANS_LOW = 3'b100, DATA_RECV_HIGH=3'b101, DATA_RECV_LOW=3'b110, QPI_TRANS=3'b111;
+  // state for qpi (read)
+  localparam RIDLE=2'b00, READ_HIGH = 2'b1, READ_LOW = 2'b10;
   reg [3:0] cnt;
 
   reg [7:0] cmd_read;
   reg [23:0] addr_buf;
-  // reg [23:0] addr_inc;
+  reg [23:0] addr_qpi;
 
   // psram
   reg [7:0] psram_mem [(1<<24)-1:0];
   // output
-  // assign dio = (rstate==RIDLE) ? 4'bz :
-  //           (rstate==READ_HIGH) ? psram_mem[addr_inc][7:4] :
-  //           (rstate==READ_LOW) ? psram_mem[addr_inc][3:0] : 4'bz;
-  assign dio = (state==DATA_TRANS_HIGH) ? psram_mem[addr_buf][7:4] :
+  assign dio = qpi_flag ? ((rstate==READ_HIGH) ? psram_mem[addr_qpi][7:4] :
+            (rstate==READ_LOW) ? psram_mem[addr_qpi][3:0] : 4'bz) :
+            (state==DATA_TRANS_HIGH) ? psram_mem[addr_buf][7:4] :
             (state==DATA_TRANS_LOW) ? psram_mem[addr_buf][3:0] : 4'bz;
   
   // command parse state machine
@@ -34,14 +39,30 @@ module psram(
     end else begin
       case (state)
         IDLE: begin
-          if (cnt < 8) begin
-            cmd_read <= {cmd_read[6:0], dio[0]};
-            cnt <= cnt + 1;
-          end
-          else begin
-            cnt <= 4'b1;
-            addr_buf <= {addr_buf[19:0], dio};
-            state <= ADDR_READ;
+          if (qpi_flag==1) begin // QPI
+            if (cnt < 2) begin
+              cmd_read <= {cmd_read[3:0], dio};
+              cnt <= cnt + 1;
+            end
+            else begin
+              cnt <= 4'b1;
+              addr_buf <= {addr_buf[19:0], dio};
+              state <= ADDR_READ;
+            end
+          end else begin // Q-SPI
+            if (cnt < 8) begin
+              cmd_read <= {cmd_read[6:0], dio[0]};
+              cnt <= cnt + 1;
+            end
+            else if (cmd_read==8'h35) begin
+              qpi_flag <= 1;
+              state <= IDLE;
+            end
+            else begin
+              cnt <= 4'b1;
+              addr_buf <= {addr_buf[19:0], dio};
+              state <= ADDR_READ;
+            end
           end
         end 
         ADDR_READ: begin
@@ -67,7 +88,12 @@ module psram(
             cnt <= cnt + 1;
           end
           else begin
-            state <= DATA_TRANS_HIGH;
+            if (qpi_flag) begin
+              state <= QPI_TRANS; // enable qpi (negedge triger)
+            end 
+            else begin
+              state <= DATA_TRANS_HIGH;
+            end
           end
         end
         DATA_TRANS_HIGH: begin
@@ -77,7 +103,7 @@ module psram(
           addr_buf <= addr_buf + 1;
           state <= DATA_TRANS_HIGH;
         end
-        DATA_RECV_HIGH: begin
+        DATA_RECV_HIGH: begin // qpi is the same as q-spi for receive
           psram_mem[addr_buf][7:4] <= dio;
           state <= DATA_RECV_LOW;
         end
@@ -86,34 +112,34 @@ module psram(
           addr_buf <= addr_buf + 1;
           state <= DATA_RECV_HIGH;
         end
+        QPI_TRANS: state <= QPI_TRANS;
         default: state <= IDLE;
       endcase
     end
   end
-  // read state machine
-  // always @(negedge sck, posedge ce_n) begin
-  //   if (ce_n) begin
-  //     addr_inc <= 0;
-  //     rstate <= 0;
-  //   end
-  //   else begin
-  //     if (state == DATA_TRANS) begin
-  //       case (rstate)
-  //         RIDLE: begin
-  //           addr_inc <= addr_buf;
-  //           rstate <= READ_HIGH;
-  //         end 
-  //         READ_HIGH: begin
-            
-  //           rstate <= READ_LOW;
-  //         end
-  //         READ_LOW: begin
-  //           addr_inc <= addr_inc + 1;
-  //           rstate <= READ_HIGH;
-  //         end
-  //         default: rstate <= RIDLE;
-  //       endcase
-  //     end
-  //   end
-  // end
+  // QPI read state machine
+  always @(negedge sck, posedge ce_n) begin
+    if (ce_n) begin
+      addr_qpi <= 0;
+      rstate <= RIDLE;
+    end
+    else begin
+      if (state == QPI_TRANS) begin
+        case (rstate)
+          RIDLE: begin
+            addr_qpi <= addr_buf;
+            rstate <= READ_HIGH;
+          end 
+          READ_HIGH: begin
+            rstate <= READ_LOW;
+          end
+          READ_LOW: begin
+            addr_qpi <= addr_qpi + 1;
+            rstate <= READ_HIGH;
+          end
+          default: rstate <= RIDLE;
+        endcase
+      end
+    end
+  end
 endmodule
