622faaafd9c6e5f2f5c2519dcc13a5e8d9ae5c82
100018556-Li Haoyan <1620774239@qq.com>
2026-04-25 19:08:05 +0800

    fix:proper QSPI, last one is incorrect

--- perip/psram/psram.v
+++ perip/psram/psram.v
@@ -5,22 +5,24 @@ module psram(
 );
 
   reg [2:0] state;
-  reg [1:0] rstate;
-  localparam IDLE = 3'b0, ADDR_READ=3'b1, WAIT=3'b10, DATA_TRANS=3'b11, 
-    DATA_RECV_HIGH=3'b100, DATA_RECV_LOW=3'b101;
-  localparam RIDLE=2'b00, READ_HIGH = 2'b1, READ_LOW = 2'b10;
+  // reg [1:0] rstate;
+  localparam IDLE = 3'b0, ADDR_READ=3'b1, WAIT=3'b10, DATA_TRANS_HIGH=3'b11,
+    DATA_TRANS_LOW = 3'b100, DATA_RECV_HIGH=3'b101, DATA_RECV_LOW=3'b110;
+  // localparam RIDLE=2'b00, READ_HIGH = 2'b1, READ_LOW = 2'b10;
   reg [3:0] cnt;
 
   reg [7:0] cmd_read;
   reg [23:0] addr_buf;
-  reg [23:0] addr_inc;
+  // reg [23:0] addr_inc;
 
   // psram
   reg [7:0] psram_mem [(1<<24)-1:0];
   // output
-  assign dio = (rstate==RIDLE) ? 4'bz :
-            (rstate==READ_HIGH) ? psram_mem[addr_inc][7:4] :
-            (rstate==READ_LOW) ? psram_mem[addr_inc][3:0] : 4'bz;
+  // assign dio = (rstate==RIDLE) ? 4'bz :
+  //           (rstate==READ_HIGH) ? psram_mem[addr_inc][7:4] :
+  //           (rstate==READ_LOW) ? psram_mem[addr_inc][3:0] : 4'bz;
+  assign dio = (state==DATA_TRANS_HIGH) ? psram_mem[addr_buf][7:4] :
+            (state==DATA_TRANS_LOW) ? psram_mem[addr_buf][3:0] : 4'bz;
   
   // command parse state machine
   always @(posedge sck or posedge ce_n) begin
@@ -61,16 +63,19 @@ module psram(
           end
         end
         WAIT: begin
-          if (cnt < 5) begin
+          if (cnt < 6) begin
             cnt <= cnt + 1;
           end
           else begin
-            cnt <= 1;
-            state <= DATA_TRANS;
+            state <= DATA_TRANS_HIGH;
           end
         end
-        DATA_TRANS: begin
-          cnt <= cnt + 1;
+        DATA_TRANS_HIGH: begin
+          state <= DATA_TRANS_LOW;
+        end
+        DATA_TRANS_LOW: begin
+          addr_buf <= addr_buf + 1;
+          state <= DATA_TRANS_HIGH;
         end
         DATA_RECV_HIGH: begin
           psram_mem[addr_buf][7:4] <= dio;
@@ -86,29 +91,29 @@ module psram(
     end
   end
   // read state machine
-  always @(negedge sck, posedge ce_n) begin
-    if (ce_n) begin
-      addr_inc <= 0;
-      rstate <= 0;
-    end
-    else begin
-      if (state == DATA_TRANS) begin
-        case (rstate)
-          RIDLE: begin
-            addr_inc <= addr_buf;
-            rstate <= READ_HIGH;
-          end 
-          READ_HIGH: begin
+  // always @(negedge sck, posedge ce_n) begin
+  //   if (ce_n) begin
+  //     addr_inc <= 0;
+  //     rstate <= 0;
+  //   end
+  //   else begin
+  //     if (state == DATA_TRANS) begin
+  //       case (rstate)
+  //         RIDLE: begin
+  //           addr_inc <= addr_buf;
+  //           rstate <= READ_HIGH;
+  //         end 
+  //         READ_HIGH: begin
             
-            rstate <= READ_LOW;
-          end
-          READ_LOW: begin
-            addr_inc <= addr_inc + 1;
-            rstate <= READ_HIGH;
-          end
-          default: rstate <= RIDLE;
-        endcase
-      end
-    end
-  end
+  //           rstate <= READ_LOW;
+  //         end
+  //         READ_LOW: begin
+  //           addr_inc <= addr_inc + 1;
+  //           rstate <= READ_HIGH;
+  //         end
+  //         default: rstate <= RIDLE;
+  //       endcase
+  //     end
+  //   end
+  // end
 endmodule
